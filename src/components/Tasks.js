62b9b878e27acabf8a4000faf655e5f2db020874
LoreMatey <lorena.matey.l@gmail.com>
2021-11-25 15:40:09 +0100

    Created Tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,36 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Meeting with friends',
+        day:'Nov 29th at 8:00 pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Vet appointment',
+        day: 'Dec 9th at 2:00 pm',
+        reminder: 'true',
+    },
+    {
+        id: 3,
+        text: 'Soul Garden concert',
+        day: 'Dec 17th at 9:30 pm',
+        reminder: 'false',
+    },
+    // {
+    //     id:
+    //     text:
+    //     day:
+    //     reminder: 
+    // }
+]
+
+const Tasks = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Tasks
