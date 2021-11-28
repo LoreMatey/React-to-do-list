7eac171b091eec11732858de9ac75bb00bf8d2d8
LoreMatey <lorena.matey.l@gmail.com>
2021-11-28 20:36:11 +0100

    Created delete function and added No tasks to show message

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,14 @@
 import { AiFillDelete } from 'react-icons/ai'
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
   return (
     <div className='task'>
       <h2>
         {task.title}
-        <AiFillDelete className='deletion-icon' />
+        <AiFillDelete 
+          className='deletion-icon' 
+          onClick={() => onDelete(task.id)} 
+        />
       </h2>
       <p>{task.day}</p>
     </div>
