b43acfe6201d3ab2bd030cd97d04b85c01846efb
LoreMatey <lorena.matey.l@gmail.com>
2021-11-29 18:12:45 +0100

    Created AddTask component with add-task form and added styles. Created custom chechbox.

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -9,9 +9,9 @@ const AddTask = () => {
         <label>Day & Time</label>
         <input type='text' placeholder='Add day and time' />
       </div>
-      <div className='form-control'>
-        <label>Set reminder</label>
-        <input type='checkbox' />
+      <div className='form-control checkbox-container'>
+        <input type='checkbox' id='custom'/>
+        <label htmlFor='custom'>Set reminder</label>
       </div>
       <input type='submit' value='Save Task' />
     </form>
